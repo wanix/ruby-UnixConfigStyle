d94e2fb91b8101a431ac7ea8c2d8f374b7252453
Erwan SEITE <wanix.fr@gmail.com>
2014-05-09 15:45:52 +0200

    continue working

--- lib/unixconfigstyle.rb
+++ lib/unixconfigstyle.rb
@@ -24,35 +24,46 @@ class UnixConfigStyle
   @@regexpStruct[:paramline2quote] = /^\s*([\d\w-_]+)\s*=\s*(".*")\s*([#;].*)?$/
   @@regexpStruct[:paramlinenormal] = /^\s*([\d\w-_]+)\s*=\s*(.*?)\s*([#;].*)?$/
 
-  # (private) Initialise the class and read the file
+  #(private) Initialise the class and read the file
+  #Parameters:
+  # config_file: String (path to a readable unix config file) (optionnal)
   def initialize(config_file=nil)
     @sections = {}
 
     if(config_file)
       push_unix_config_file(config_file)
     end
-  end
+  end #initialize
   private :initialize
 
-  # (private) Validate a file is readable
+  #(private) Validate a file is readable
+  #Parameters:
+  # fileToRead: String (path to a readable file)
   def validate(fileToRead)
     unless File.readable?(fileToRead)
       raise Errno::EACCES, "Can't read #{fileToRead}"
     end
-  end
+  end #validate
   private :validate
 
-  # read a config file and add its properties to the end of the object ones
+  #Read a config file and add its properties to the end of the object ones
+  #Parameters:
+  # config_file: String (path to a readable unix config file)
   def push_unix_config_file(config_file)
     add_unix_config_file(config_file,"push")
-  end
+  end #push_unix_config_file
 
-  # read a config file and add its properties to the start of the object ones
+  #Read a config file and add its properties to the start of the object ones
+  #Parameters:
+  # config_file: String (path to a readable unix config file)
   def insert_unix_config_file(config_file)
     add_unix_config_file(config_file,"insert")
-  end
+  end #insert_unix_config_file
 
-  # (private) read a config file and add its properties to the current object
+  #(private) read a config file and add its properties to the current object
+  #Parameters:
+  # config_file: String (path to a readable unix config file)
+  # operation: String [push|insert]
   def add_unix_config_file(config_file, operation="push")
     if operation != "push" and operation != "insert"
       raise ArgumentError, "operation #{operation} not allowed", caller
@@ -88,7 +99,7 @@ class UnixConfigStyle
           if operation == "push"
             @sections[currentSection][paramlinematch[1]].push(parammatch[2])
           elsif operation == "insert"
-            @sections[currentSection][paramlinematch[1]].insert(1,parammatch[2])
+            @sections[currentSection][paramlinematch[1]].insert(0,parammatch[2])
           end
           next
         end
@@ -97,7 +108,7 @@ class UnixConfigStyle
           if operation == "push"
             @sections[currentSection][paramlinematch[1]].push(parammatch[2])
           elsif operation == "insert"
-            @sections[currentSection][paramlinematch[1]].insert(1,parammatch[2])
+            @sections[currentSection][paramlinematch[1]].insert(0,parammatch[2])
           end
           next
         end
@@ -106,7 +117,7 @@ class UnixConfigStyle
           if operation == "push"
             @sections[currentSection][paramlinematch[1]].push(parammatch[2])
           elsif operation == "insert"
-            @sections[currentSection][paramlinematch[1]].insert(1,parammatch[2])
+            @sections[currentSection][paramlinematch[1]].insert(0,parammatch[2])
           end
           next
         end
@@ -114,18 +125,33 @@ class UnixConfigStyle
         warn("[warning] unrecognized line "+file.lineno.to_s+" in "+@unix_config_file+" : "+line)
       end
     end
+    file.close()
   end
   private :add_unix_config_file
 
-  # For debugging purpose : print the object in a unix config style
-  def print(comment=nil)
+  #Write the object in a file
+  #Parameters:
+  # fileToWrite : String, path to a file
+  # comment: String (optionnal)
+  # TODO
+  #def write (fileToWrite, comment=nil)
+  #  File.open(fileToWrite, 'w') do |file|
+  #    self.print (comment, file.to_io)
+  #  end
+  #end #def write
+
+  #Print the object in a unix config style into an IO obj
+  #Parameters:
+  # comment: String
+  # io_obj : IO (default stdout)
+  def print (comment=nil, io_obj=$stdout)
     if comment
-      puts "##{comment}"
+      io_obj.puts "##{comment}"
     end
     if @sections.key?(@@rootsection)
       @sections[@@rootsection].keys.each do |param_key|
         @sections[@@rootsection][param_key].each do |param_value|
-          puts "#{param_key}=#{param_value}"
+          io_obj.puts "#{param_key}=#{param_value}"
         end
       end
     end
@@ -133,43 +159,103 @@ class UnixConfigStyle
       if section_key == @@rootsection
         next
       end
-      puts "[#{section_key}]"
+      io_obj.puts "[#{section_key}]"
       @sections[section_key].keys.each do |param_key|
         @sections[section_key][param_key].each do |param_value|
-          puts "#{param_key}=#{param_value}"
+          io_obj.puts "#{param_key}=#{param_value}"
         end
       end
     end
-  end #def puts
+  end #def print
 
-  #Get a param array
-  def getParams (key, section=@@rootsection)
+  #Get all avalaibles sections (all but root one)
+  #return an array
+  def getSections ()
+    allsections = @sections.keys
+    if allsections
+      allsections.delete(@@rootsection)
+    end
+    return allsections
+  end #def getSections
+
+  #Get all avalaibles keys for this section
+  #return an array (or nil if no keys found)
+  #Parameters:
+  # section: String (optionnal)
+  def getKeys (section=@@rootsection)
+    if @sections.key?(section)
+      return @sections[section].keys
+    end
+    return nil
+  end #def getKeys
+
+  #Add values for a key
+  #Parameters:
+  # values: Array or String
+  # key: String
+  # section: String (optionnal)
+  def addValues (values, key, section=@@rootsection)
+    @sections[section]={} unless @sections.key?(section)
+    @sections[section][key]=[] unless @sections[section].key?(key)
+    if @sections[section][key].push(values)
+      return true
+    else
+      return false
+    end
+  end #def addValues
+
+  #Insert values for a key
+  #Parameters:
+  # values: Array or String
+  # key: String
+  # section: String (optionnal)
+  def insertValues (values, key, section=@@rootsection)
+    @sections[section]={} unless @sections.key?(section)
+    @sections[section][key]=[] unless @sections[section].key?(key)
+    if @sections[section][key].insert(0,values)
+      return true
+    else
+      return false
+    end
+  end #insertValues
+
+  #Get all values for a key (in a section if given, else in root one)
+  #Parameters:
+  # key: String
+  # section: String (optionnal)
+  def getValues (key, section=@@rootsection)
     if @sections.key?(section)
       if @sections[section].key?(key)
         return @sections[section][key]
       end
     end
     return nil
-  end
+  end #getValues
 
-  #Get the first param for a key (config first win)
-  def getFirstParam (key, section=@@rootsection)
+  #Get the first value for a key (config first win)
+  #Parameters:
+  # key: String
+  # section: String (optionnal)
+  def getFirstValue (key, section=@@rootsection)
     if @sections.key?(section)
       if @sections[section].key?(key)
         return @sections[section][key].first
       end
     end
     return nil
-  end
+  end #getFirstValue
 
-  #Get the last param for a key (config last win)
-  def getLastParam (key, section=@@rootsection)
+  #Get the last value for a key (config last win)
+  #Parameters:
+  # key: String
+  # section: String (optionnal)
+  def getLastValue (key, section=@@rootsection)
     if @sections.key?(section)
       if @sections[section].key?(key)
         return @sections[section][key].last
       end
     end
     return nil
-  end
+  end #getLastValue
 
 end
